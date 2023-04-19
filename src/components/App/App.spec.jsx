9210b4e19c5d3a2fcd9b9423094e2a3abf2fbaf9
Elias gabriel <elias.gabriel@agenciam3.com>
2023-04-19 17:32:42 -0300

    adicionado babel, jest e library testing, ainda não esta compilando jsx para ts!

--- src/components/App/App.spec.jsx
+++ src/components/App/App.spec.jsx
@@ -1,3 +1,38 @@
+// import { render } from '@testing-library/react';
+// import App from '../App/App';
+
+// describe('App', () => {
+//   it('renders the header', () => {
+//     const { getByRole } = render(<App />);
+//     const headerElement = getByRole('banner');
+//     expect(headerElement).toBeInTheDocument();
+//   });
+
+//   it('renders the slider', () => {
+//     const { getByRole } = render(<App />);
+//     const sliderElement = getByRole('slider');
+//     expect(sliderElement).toBeInTheDocument();
+//   });
+
+//   it('renders the product slider', () => {
+//     const { getByRole } = render(<App />);
+//     const productSliderElement = getByRole('list');
+//     expect(productSliderElement).toBeInTheDocument();
+//   });
+
+//   it('renders the newsletter', () => {
+//     const { getByRole } = render(<App />);
+//     const newsletterElement = getByRole('form');
+//     expect(newsletterElement).toBeInTheDocument();
+//   });
+
+//   it('renders the footer', () => {
+//     const { getByRole } = render(<App />);
+//     const footerElement = getByRole('contentinfo');
+//     expect(footerElement).toBeInTheDocument();
+//   });
+// });
+
 describe('Jest', () => {
     it('testing jest', () => {
         expect(1).toBe(1);
